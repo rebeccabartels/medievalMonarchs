694683fbb42c970e2c5e8b43b69cecf285e9c277
rebeccabartels <rebeccakbartels@icloud.com>
2016-08-29 11:43:56 -0400

    bloop

--- MedievalMonarchs/TableViewController.swift
+++ MedievalMonarchs/TableViewController.swift
@@ -14,6 +14,7 @@ class TableViewController: UITableViewController {
         print(englishHouses)
     }
     
+    
     func readJsonFromFile() -> NSDictionary{
         var returnDict = [:]
         
